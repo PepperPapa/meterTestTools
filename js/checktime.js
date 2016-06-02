68607af229ce5183def5ec850b60b7406bd57710
pepperpapa <zhongxin2506@outlook.com>
2016-06-02 10:33:06 +0800

    1.update css style; 2.update checktime.js for match YYMMDDhhmmss time format data

--- js/checktime.js
+++ js/checktime.js
@@ -1,5 +1,6 @@
-// 检查310抄读出的时间数据YYMMDDhhmm是否符合格式要求
+// 检查310抄读出的时间数据是否符合格式要求
 // 主要用于需量发生时间、冻结时间等包含大量时间数据格式的检查
+// 310抄读的时间数据存在两种格式：YYMMDDhhmm、YYMMDDhhmmss
 // 测试方法：
 // 1.使用310抄读相应的数据项，到处至excel，纵向排列
 // 2.使用excel筛选出所有包含时间的数据YYMMDDhhmm
@@ -14,13 +15,14 @@ run.onclick = function() {
 
     var result = "";
 
-    // 执行检查 YYMMDDhhmm
+    // 执行检查 YYMMDDhhmm、YYMMDDhhmmss
     // 1.长度为 10
     // 2.YY范围：数字即可
     // 3.MM范围：01～12
     // 4.DD范围: 01~31   未考虑闰月等情况
     // 5.hh范围: 00～23
     // 6.mm范围: 00～59
+    // 7.ss范围：00～59
 
     for (var i in all_time) {
       var error = false;
@@ -28,9 +30,10 @@ run.onclick = function() {
       var MM = all_time[i].slice(2, 4);
       var DD = all_time[i].slice(4, 6);
       var hh = all_time[i].slice(6, 8);
-      var mm = all_time[i].slice(8);
+      var mm = all_time[i].slice(8, 10);
+      var ss = all_time[i].slice(10, 12);
 
-      if (all_time[i].length !== 10) {
+      if (all_time[i].length !== 10 && all_time[i].length !== 12) {
         error = true;
       } else if ( !( /\d{2}/g.test(YY) ) ) {
         error = true;
@@ -42,9 +45,13 @@ run.onclick = function() {
         error = true;
       } else if (mm < 0 | mm > 59) {
         error = true;
+      } else if (all_time[i].length === 12) {   // YYMMDDhhmmss 此格式下需检查秒
+        if (ss < 0 | ss > 59) {
+          error = true;
+        }
       }
       // 0000000000 为合法数据，表示未发生
-      if (all_time[i] == "0000000000") {
+      if (all_time[i] === "0000000000") {
         error = false;
       }
 
